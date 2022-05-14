4558f052bb521b372d4eaab2d9c9142754e65000
dylan <dylan@simple-salt.com>
2022-05-14 08:01:56 -0500

    contact v36

---
+++ functions/_middleware.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "saltyonsecurity contact", email: "contactform@saltyonsecurity.net" }],
+    },
+  ],
+  from: { name: "saltyonsecurity contact page", email: "contactform@saltyonsecurity.net" },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/thank-you" },
+    }),
+});
